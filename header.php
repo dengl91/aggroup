7e61923c295337aa21bd4bccd6f87ad9783ec46e
dengl91 <denisflip@gmail.com>
2021-10-11 16:59:29 +0300

    FCP improvement

--- header.php
+++ header.php
@@ -31,6 +31,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 
+	<?php if ( !isBot() ) : ?>
 	<style>
 		/* latin */
 		@font-face {
@@ -60,6 +61,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 		}
 	</style>
+	<?php endif; ?>
 
 	<?php wp_head(); ?>
 </head>
